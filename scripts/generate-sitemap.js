9076c48927c03f3493b51a69275922aa7df4d13a
Ahmed bahaadine <geekbahaa1@gmail.com>
2025-11-30 01:24:41 +0200

    Update sitemap generation and add product URLs for localization
    - Adjusted the sitemap generation script to include product URLs for all supported locales, enhancing localization support.
    - Modified the product fetching logic to handle smaller page sizes and added timeout handling to prevent query delays.
    - Increased pagination limits to accommodate more products and added a delay to avoid overwhelming the database.
    - Improved error handling to return fetched products even in case of errors, ensuring better resilience in the sitemap generation process.

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:088ff767a4b7fc77caf5e9538dcc586e2e960c5abc2ff374abffad1a78fe0fc3
-size 12337
+oid sha256:023ee55d1da0bed0c42621f6ba1f3ccbe680b0ff75ee30ae018951f26ee91b7d
+size 14084
